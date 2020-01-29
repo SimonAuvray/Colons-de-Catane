386ade25d0e52c2ed61cab58bace26b31c0e45ce
Simon Auvray <simon.auvray@soprasteria.com>
2020-01-29 11:43:34 +0100

    link Tuile database
    Les couleurs des tuiles correspondent à leur ressource

--- colonscatane-web/src/main/java/fr/colonscatane/controller/PartieController.java
+++ colonscatane-web/src/main/java/fr/colonscatane/controller/PartieController.java
@@ -1,18 +1,22 @@
 package fr.colonscatane.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.swing.JOptionPane;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.colonscatane.dao.IDAOJoueur;
+import fr.colonscatane.dao.IDAOTuileRessource;
 import fr.colonscatane.modele.Joueur;
 import fr.colonscatane.modele.ROLE;
+import fr.colonscatane.modele.TuileRessource;
 
 @Controller
 public class PartieController {
@@ -20,8 +24,21 @@ public class PartieController {
 	@Autowired
 	private IDAOJoueur daoJoueur;
 	
-	@GetMapping("partie")
-	public String lancerpartie() {
+	@Autowired
+	private IDAOTuileRessource daoTuile;
+	
+	
+	
+	@GetMapping("/partie")
+	public String lancerpartie(Model model) {
+		
+		List<TuileRessource> mesTuiles =  daoTuile.findAll();
+		
+		for (TuileRessource t : mesTuiles) {
+			model.addAttribute("Tuile"+t.getId(), t);
+			
+			//System.out.println( "RessourceTuile"+t.getId() +" "+t.getType().toString());
+		}
 		return "partie";
 	}
 

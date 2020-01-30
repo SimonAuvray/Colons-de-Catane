a1fbb21d53856b52f5046adbc160a7729137ede9
aballenghien <audrey.ballenghien@gmail.com>
2020-01-30 09:35:43 +0100

    fin de rebase, WIP merge partie html

--- colonscatane-web/src/main/java/fr/colonscatane/controller/MenuController.java
+++ colonscatane-web/src/main/java/fr/colonscatane/controller/MenuController.java
@@ -3,70 +3,50 @@ package fr.colonscatane.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
-
-
-import javax.swing.JOptionPane;
 import javax.transaction.Transactional;
 
-
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.annotation.ApplicationScope;
-import org.springframework.web.context.annotation.SessionScope;
-
-
-
 import fr.colonscatane.application.PartieContextLoader;
 import fr.colonscatane.dao.IDAOJoueur;
 import fr.colonscatane.dao.IDAOPartie;
-import fr.colonscatane.dao.IDAOUtilisateur;
 import fr.colonscatane.modele.Joueur;
 import fr.colonscatane.modele.Partie;
 import fr.colonscatane.modele.ROLE;
-import fr.colonscatane.modele.Utilisateur;
 
 @Controller
 public class MenuController {
-	
+
 	@Autowired
 	private IDAOJoueur daoJoueur;
-	
+
 	@Autowired
-<<<<<<< HEAD
 	private IDAOPartie daoPartie;
-	
+
 	@Autowired
 	private PartieContextLoader partieContext;
-  
-  @Autowired
-	private IDAOUtilisateur<Utilisateur> daoUtilisateur;
-	
+
+
 	@GetMapping("/menu")
 	public String getMenu() {
 		return "menu";
 	}
-	
+
 	@GetMapping("/nouvellepartie")
 	@Transactional
-	public String getNouvellePartie(Model model,
-			HttpSession session) {
-		
-		//enregistrer l'utilisateur de session comme joueur immediatement
+	public String getNouvellePartie(Model model, HttpSession session) {
+
+		// enregistrer l'utilisateur de session comme joueur immediatement
 		Joueur monJoueur = daoJoueur.findByUsername((String) session.getAttribute("user")).orElse(null);
 		monJoueur.setRole(ROLE.Joueur);
-		
-		//creation de la partie, ajout de l'utilisateur (si pas deja fait) et enregistrement de la nouvelle liste Joueurs
-		if(monJoueur.getPartie() == null) {
+
+		// creation de la partie, ajout de l'utilisateur (si pas deja fait) et
+		// enregistrement de la nouvelle liste Joueurs
+		if (monJoueur.getPartie() == null) {
 			Partie maPartie = new Partie();
 			List<Joueur> listeJoueurs = new ArrayList<Joueur>();
 			listeJoueurs.add(monJoueur);
@@ -74,25 +54,25 @@ public class MenuController {
 			maPartie = daoPartie.save(maPartie);
 			monJoueur.setPartie(maPartie);
 			monJoueur = daoJoueur.save(monJoueur);
-			//enregistrement de la partie dans le scope application
+			// enregistrement de la partie dans le scope application
 			List<Partie> listeParties = new ArrayList<Partie>();
 			listeParties.add(maPartie);
-			partieContext.setParties(listeParties);			
+			partieContext.setParties(listeParties);
 		}
 		int i = 0;
-		//recuperation du joueur mis a jour avec sa partie, pour chaque joueur de la partie envoyer dans la session pour affichage
+		// recuperation du joueur mis a jour avec sa partie, pour chaque joueur de la
+		// partie envoyer dans la session pour affichage
 		Partie maPartie = daoPartie.findByIdFetchingJoueurs(monJoueur.getPartie().getId());
 //		Partie maPartie = daoPartie.findById(monJoueur.getPartie().getId()).get();
 //		Hibernate.initialize(maPartie.getLstJoueurs());
 		List<Joueur> mesJoueurs = maPartie.getLstJoueurs();
-		for(Joueur j : mesJoueurs) {
+		for (Joueur j : mesJoueurs) {
 			model.addAttribute("joueur" + i, j);
 			i++;
-		}	
+		}
 		return "nouvellepartie";
 	}
-		
-	
+
 	@GetMapping("/parametres")
 	public String getParametres() {
 		return "parametres";
